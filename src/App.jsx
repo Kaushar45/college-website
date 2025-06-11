1f1216e983c11e5190f2e03a02e7c5f19b73a345
Kaushar45 <45kaushar@gmail.com>
2025-06-11 20:32:11 +0530

    project setup with react + vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello,I am Home Page</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <h2>My College</h2>
+        <nav></nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
