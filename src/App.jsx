a21c83c3553e32ab4d293ac78772466d825af1c8
Kaushar45 <45kaushar@gmail.com>
2025-06-11 20:51:03 +0530

    added react router & tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Hello,I am Home Page</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admission" element={<Admission />} />
+      </Routes>
     </>
   );
 }
